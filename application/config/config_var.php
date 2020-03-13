4bb8b4444270b9e0609e3e92a315b0c39b527b15
Andi Hafiidh <andi.hafiidh@gmail.com>
2020-03-14 00:06:05 +0700

    Add config_var for custom Const or Config

---
+++ application/config/config_var.php
@@ -0,0 +1,8 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+date_default_timezone_set('Asia/Jakarta');
+
+define('FORMAT_RESULT','json');
+
+$config['default_content_type'] = 'application/json';
