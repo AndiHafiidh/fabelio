834a51e94258daa26201329610f459fa26e857e1
Andi Hafiidh <andi.hafiidh@gmail.com>
2020-03-15 02:46:22 +0700

    Add const for API_URL Fabelio

--- application/config/config_var.php
+++ application/config/config_var.php
@@ -4,5 +4,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 date_default_timezone_set('Asia/Jakarta');
 
 define('FORMAT_RESULT','json');
+// define('APP_URL','http://fabelio.id');
+define('APP_URL','http://fabelio.id');
+
 
 $config['default_content_type'] = 'application/json';
